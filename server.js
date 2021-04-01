f91f676833e6b228f7819bf0cfec9d0aa3983207
ArnoldPires <arnold.pires92@gmail.com>
2021-04-01 11:15:43 -0400

    stuff

--- server.js
+++ server.js
@@ -1,14 +1,13 @@
 const express = require("express");
 const app = express();
 const MongoClient = require("mongodb").MongoClient;
-const PORT = 8000;
-require("dotenv").config();
-
-app.use(cors());
+const PORT = 3000;
+const uri = process.env.MONGODB_URI;
 
 let db,
-  dbConnectionStr = process.env.DB_STRING,
-  dbName = "ClusterHiking";
+  dbConnectionStr =
+    "mongodb+srv://arnaldo:HfFxTWP91Zc4fugt@clusterhiking.g3zd7.mongodb.net/Hiking?retryWrites=true&w=majority",
+  dbName = "todoList";
 
 MongoClient.connect(dbConnectionStr, { useUnifiedTopology: true }).then(
   (client) => {
@@ -17,15 +16,43 @@ MongoClient.connect(dbConnectionStr, { useUnifiedTopology: true }).then(
   }
 );
 
-app.set("view engine", "ejs");
+app.set("view-engine", "ejs");
 app.use(express.static("public"));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 app.get("/", (request, response) => {
-  response.sendFile(__dirname + "views/index.ejs");
+  db.collection("chores")
+    .find()
+    .toArray()
+    .then((data) => {
+      response.render("index.ejs", { info: data });
+    })
+    .catch((error) => console.log(error));
+});
+
+app.post("/addItem", (request, response) => {
+  db.collection("chores")
+    .insertOne(request.body)
+    .then((result) => {
+      console.log("Added Item");
+      response.redirect("/");
+    })
+    .catch((error) => {
+      console.log(error);
+    });
+});
+
+app.delete("/deleteItem", (request, response) => {
+  db.collection("chores")
+    .deleteOne({ todo: request.body.todoX })
+    .then((result) => {
+      console.log("Deleted Item");
+      response.json("Deleted Item");
+    })
+    .catch((error) => console.log(error));
 });
 
 app.listen(process.env.PORT || PORT, () => {
-  console.log(`server is running on port ${PORT}`);
-}); 
+  console.log(`Server running on port ${PORT}`);
+});
