6a6f8b50bb5438b217cfda0efa3afbe862b4b635
ArnoldPires <arnold.pires92@gmail.com>
2021-11-12 15:39:01 -0500

    some small changes

--- controllers/auth.js
+++ controllers/auth.js
@@ -14,14 +14,14 @@ exports.getLogin = (req, res) => {
 exports.postLogin = (req, res, next) => {
     const validationErrors = []
     if (!validator.isEmail(req.body.email)) {
-        validationErrors.push( {msg: 'Not sure if that email is valid. Try another one.'})
+        validationErrors.push({ msg: 'Not sure if that email is valid. Try another one.'})
     }
     if (validator.isEmpty(req.body.password)) {
         validationErrors.push({ msg: 'You gotta enter in a password to continue.'})
     }
     if (validationErrors.length) {
         req.flash("errors", validationErrors)
-        return res.redirect("/login")
+        return res.redirect("/login");
     }
     req.body.email = validator.normalizeEmail(req.body.email, { gmail_remove_dots: false})
     passport.authenticate('local', (err, user, info) => {
@@ -33,7 +33,7 @@ exports.postLogin = (req, res, next) => {
         req.logIn(user, (err) => {
             console.error(err)
             if (err) { return next(err) }
-            req.flash('success', {msg: "You've logged in."})
+            req.flash('success', { msg: "You've logged in."})
             res.redirect(req.session.returnTo || '/profile')
         })
     }) (req, res, next)
@@ -79,13 +79,13 @@ exports.postSignup = (req, res, next) => {
         password: req.body.password
     })
     User.findOne({ email: req.body.email }, (err, existingUser) => {
-        if (err) {return next(err)}
+        if (err) { return next(err) }
         if (existingUser) {
             req.flash('errors', {msg: "An account with that email adress already exists."})
             return res.redirect("/signup")
         }
         user.save(err => {
-            if (err) {return next(err)}
+            if (err) { return next(err) }
             req.logIn(user, (err) => {
                 if (err) {
                     return next(err)
