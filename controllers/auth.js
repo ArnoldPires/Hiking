46a7ed3979a9a72b7b5b45d8416d72289c98b5b8
ArnoldPires <arnold.pires92@gmail.com>
2021-11-11 19:20:16 -0500

    again x2

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,6 +1,6 @@
 const passport = require('passport')
 const validator = require('validator')
-const User = require('../../Hiking/models/User')
+import User from '../models/User';
 
 exports.getLogin = (req, res) => {
     if (req.user) {
